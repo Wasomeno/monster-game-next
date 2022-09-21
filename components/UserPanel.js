ba428009ffe14d70f3c2b9a856434047b912f951
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-09-21 20:37:40 +0700

    Add functions for getting user details and status

--- components/UserPanel.js
+++ components/UserPanel.js
@@ -1,26 +1,46 @@
 import React, { useEffect, useState, useContext } from "react";
-import { ethers } from "ethers";
-import { itemsContract } from "../helpers/contractConnection";
+import { itemsContract, usersDataContract } from "../hooks/useContract";
+import { BigNumber, ethers } from "ethers";
 import InventoryModal from "./InventoryModal";
 import MonstersModal from "./MonstersModal";
 import AppContext from "./AppContext";
+import { useUserStatus } from "../hooks/useAccount";
 
 const UserPanel = () => {
   const connection = useContext(AppContext);
   const [showInventory, setShowInventory] = useState(false);
   const [showMonsters, setShowMonsters] = useState(false);
   const [gold, setGold] = useState(0);
+  const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(true);
   const itemsHandler = itemsContract();
+  const userDataHandler = usersDataContract();
 
+  async function getUserStatus() {
+    const result = await useUserStatus(connection.account[0]);
+    setIsRegistered(result);
+  }
   async function getGold() {
-    await itemsHandler.balanceOf(connection.account[0], 0).then((response) => {
-      setGold(response.toString());
+    await itemsHandler.balanceOf(connection.account[0], 0).then((golds) => {
+      setGold(golds.toString());
     });
+  }
+  async function getUserDetails() {
+    await userDataHandler
+      .userDataDetails(connection.account[0])
+      .then((details) => {
+        setDetails(details);
+        setLoading(false);
+      });
+  }
+
+  function bytesToString(string) {
+    return ethers.utils.parseBytes32String(string);
   }
 
   useEffect(() => {
-    getGold();
-  }, []);
+    getUserDetails();
+  }, [details.length]);
 
   return (
     <>
@@ -30,25 +50,29 @@ const UserPanel = () => {
         style={{ background: "rgba(66, 63, 62, 0.5)" }}
       >
         <h5 id="text" className="p-1 text-white">
-          {connection.account[0]
-            ? connection.account[0].slice(0, 6) +
-              "..." +
-              connection.account[0].slice(36, 42)
-            : "Unknown"}
+          {loading ? "loading....." : bytesToString(details.name)}
         </h5>
         <div
           id="user-image"
-          style={{
-            backgroundImage: `url(${process.env.PUBLIC_URL + "user-icon.png"})`,
-          }}
+          style={
+            loading
+              ? { backgroundColor: "#000" }
+              : {
+                  backgroundImage: `url(${
+                    "/profile/profile_" +
+                    bytesToString(details.profile_image) +
+                    ".png"
+                  })`,
+                }
+          }
         />
-        <h6 id="text" className="p-1 w-100 text-white text-center">
+        <h5 id="text" className="p-1 my-2 w-100 text-white text-center">
           Gold: {gold}
-        </h6>
-        <div id="user-menu">
-          <h5
-            id="text"
-            className="border border-dark p-1 px-2 rounded-pill text-white"
+        </h5>
+        <div id="user-menu" className="d-flex flex-column">
+          <button
+            id="user-menu-button"
+            className="border border-dark p-2 px-3 rounded-pill d-flex my-1"
             onClick={() => setShowInventory(true)}
           >
             <img
@@ -57,11 +81,11 @@ const UserPanel = () => {
               alt="inventory-icon"
               width={"18px"}
             />
-            Inventory
-          </h5>
-          <h5
-            id="text"
-            className="border border-dark p-1 px-2 rounded-pill text-white"
+            <h5 className="m-0 p-0">Inventory</h5>
+          </button>
+          <button
+            id="user-menu-button"
+            className="border border-dark p-2 px-3 rounded-pill d-flex my-1"
             onClick={() => setShowMonsters(true)}
           >
             <img
@@ -70,8 +94,8 @@ const UserPanel = () => {
               alt="inventory-icon"
               width={"18px"}
             />
-            Monsters
-          </h5>
+            <h5 className="m-0 p-0">Monsters</h5>
+          </button>
         </div>
       </div>
 
