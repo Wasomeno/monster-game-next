030dbd224cbb5e36e1c3b236b9959c00c495adbd
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-09-24 22:57:39 +0700

    Moved setLoading and setToast to context

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,13 @@ import "react-toastify/dist/ReactToastify.css";
 import Layout from "../components/Layout";
 import AppContext from "../components/AppContext";
 import { AnimatePresence } from "framer-motion";
+import { setToast } from "../components/Toast";
+import { setLoading } from "../components/LoadingScreen";
 
 function MyApp({ Component, pageProps }) {
   const [account, setAccount] = useState([]);
+  const [setToastText, setCondition, toggleToast, Toast] = setToast();
+  const [setLoadingText, toggleLoading, Loading] = setLoading();
   return (
     <AnimatePresence exitBeforeEnter={true}>
       <div className="bg-dark">
@@ -16,11 +20,21 @@ function MyApp({ Component, pageProps }) {
           value={{
             account: account,
             setAccount: setAccount,
+            loading: {
+              setLoadingText: setLoadingText,
+              toggleLoading: toggleLoading,
+            },
+            toast: {
+              setToastText: setToastText,
+              setCondition: setCondition,
+              toggleToast: toggleToast,
+            },
           }}
         >
           <Layout>
+            <Loading />
             <Component {...pageProps} />
-            <ToastContainer position="bottom-center" closeOnClick={false} />
+            <Toast />
           </Layout>
         </AppContext.Provider>
       </div>
