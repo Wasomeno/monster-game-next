a30e8a375a2e54e5d4d2dea307ae8385d5a7cdb9
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-09-26 21:45:57 +0700

    Added setLoading and setToast hooks

--- pages/_app.js
+++ pages/_app.js
@@ -8,37 +8,47 @@ import AppContext from "../components/AppContext";
 import { AnimatePresence } from "framer-motion";
 import { setToast } from "../components/Toast";
 import { setLoading } from "../components/LoadingScreen";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const [account, setAccount] = useState([]);
-  const [setToastText, setCondition, toggleToast, Toast] = setToast();
+  const [error, success, spinner, toggleSpinner, toggleToast, Toast] =
+    setToast();
   const [setLoadingText, toggleLoading, Loading] = setLoading();
   return (
-    <AnimatePresence exitBeforeEnter={true}>
-      <div className="bg-dark">
-        <AppContext.Provider
-          value={{
-            account: account,
-            setAccount: setAccount,
-            loading: {
-              setLoadingText: setLoadingText,
-              toggleLoading: toggleLoading,
-            },
-            toast: {
-              setToastText: setToastText,
-              setCondition: setCondition,
-              toggleToast: toggleToast,
-            },
-          }}
-        >
-          <Layout>
-            <Loading />
-            <Component {...pageProps} />
-            <Toast />
-          </Layout>
-        </AppContext.Provider>
-      </div>
-    </AnimatePresence>
+    <>
+      <Head>
+        <title>Monsters Game</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <AnimatePresence exitBeforeEnter={true}>
+        <div className="bg-dark">
+          <AppContext.Provider
+            value={{
+              account: account,
+              setAccount: setAccount,
+              loading: {
+                setLoadingText: setLoadingText,
+                toggleLoading: toggleLoading,
+              },
+              toast: {
+                error: error,
+                success: success,
+                spinner: spinner,
+                toggleSpinner: toggleSpinner,
+                toggleToast: toggleToast,
+              },
+            }}
+          >
+            <Layout>
+              <Loading />
+              <Component {...pageProps} />
+              <Toast />
+            </Layout>
+          </AppContext.Provider>
+        </div>
+      </AnimatePresence>
+    </>
   );
 }
 
