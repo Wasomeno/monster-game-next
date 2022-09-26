d42c23aaa3ab2aa3d2c1d03ab2ea57865bea01f6
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-09-26 21:46:52 +0700

    Fix the order in smelterContract

--- hooks/useContract.js
+++ hooks/useContract.js
@@ -84,9 +84,9 @@ export function smelterContract() {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
   const contract = new ethers.Contract(
-    signer,
     process.env.SMELTER_CONTRACT_ADDRESS,
-    SmelterABI.abi
+    SmelterABI.abi,
+    signer
   );
   return contract;
 }
