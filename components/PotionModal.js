f2d62b1f82bdf697fc8d295cd0ffb19335a7012f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-01 23:32:23 +0700

    Add: useEnergyPotion function

--- components/PotionModal.js
+++ components/PotionModal.js
@@ -1,19 +1,67 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { itemsContract, monsterGameContract } from "../hooks/useContract";
+import AppContext from "./AppContext";
+import { motion } from "framer-motion";
+import useProvider from "../hooks/useProvider";
 
-const PotionModal = () => {
-  const connection = useContext(AppContext);
+const PotionModal = ({ showPotions, setShowPotions, monster }) => {
+  const user = useContext(AppContext).account[0];
   const [amount, setAmount] = useState(1);
   const [potion, setPotion] = useState(0);
+  const toast = useContext(AppContext).toast;
+  const loading = useContext(AppContext).loading;
+  const provider = useProvider();
   const itemsHelper = itemsContract();
   const monsterGameHelper = monsterGameContract();
 
   async function getPotion() {
-    await itemsHelper.balanceOf(connection.account[0], 2).then((response) => {
+    await itemsHelper.balanceOf(user, 2).then((response) => {
       setPotion(response);
     });
   }
 
-  async function usePotion() {}
+  async function useEnergyPotion() {
+    const isApproved = await itemsHelper.isApprovedForAll(
+      user,
+      monsterGameHelper.address
+    );
+    if (!isApproved) {
+      try {
+        await itemsHelper.setApprovalAll(monsterGameHelper.address, true);
+        await monsterGameHelper
+          .useEnergyPotion(monster, amount)
+          .then((response) => {
+            loading.setLoadingText(
+              "Monster #" + monster + " Consuming potion..."
+            );
+            loading.toggleLoading();
+            provider.waitForTransaction(response.hash).then(() => {
+              loading.toggleLoading();
+              getPotion();
+            });
+          });
+      } catch (error) {
+        toast.error(error.reason);
+      }
+    } else {
+      try {
+        await monsterGameHelper
+          .useEnergyPotion(monster, amount)
+          .then((response) => {
+            loading.setLoadingText(
+              "Monster #" + monster + " Consuming potion..."
+            );
+            loading.toggleLoading();
+            provider.waitForTransaction(response.hash).then(() => {
+              loading.toggleLoading();
+              getPotion();
+            });
+          });
+      } catch (error) {
+        toast.error(error.reason);
+      }
+    }
+  }
 
   function decrement() {
     if (amount <= 1) return;
@@ -26,15 +74,15 @@ const PotionModal = () => {
 
   useEffect(() => {
     getPotion();
-  }, []);
+  }, [potion]);
 
-  if (!showFeed) return;
+  if (!showPotions) return;
   return (
     <>
       <motion.div
         id="modal-screen"
         className="h-100 w-100 bg-dark bg-opacity-75"
-        onClick={() => setShowFeed(false)}
+        onClick={() => setShowPotions(false)}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
@@ -61,7 +109,11 @@ const PotionModal = () => {
                 x {potion.toString()}
               </h5>
             </div>
-            <button className="btn btn-success" id="text">
+            <button
+              className="btn btn-success"
+              id="text"
+              onClick={useEnergyPotion}
+            >
               Use Potion
             </button>
           </div>
