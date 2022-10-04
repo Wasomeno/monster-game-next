52e62d4e44f4d9e646282289be49872d7930c813
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-04 23:10:28 +0700

    Add: chainChanged handler

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,18 @@
+import { useContext, useEffect } from "react";
 import Navigation from "./Navigations";
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    if (!window.ethereum) return;
+    window.ethereum.on("chainChanged", () => {
+      window.location.reload();
+    });
+  }, []);
+
   return (
     <>
       <Navigation />
-      <main>{children}</main>
+      {children}
     </>
   );
 };
