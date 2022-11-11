6c0b792c45497b7056c1a2d623b2932330f403cb
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-11 22:21:43 +0700

    Change: delete isConnected condition

--- components/Navigations.js
+++ components/Navigations.js
@@ -1,23 +1,12 @@
-import React, { useContext, useEffect, useState } from "react";
-import AppContext from "./AppContext";
+import React, { useContext } from "react";
+import AppContext from "../contexts/AppContext";
 import MapButton from "./MapButton";
-import NotConnected from "./NotConnected";
 import UserPanel from "./UserPanel";
 
 const Navigation = () => {
-  const connection = useContext(AppContext);
-  const account = connection.account;
-  const isConnected = Boolean(account[0]);
-
   return (
     <>
-      {!isConnected ? (
-        <NotConnected />
-      ) : (
-        <>
-          <UserPanel /> <MapButton />
-        </>
-      )}
+      <UserPanel /> <MapButton />
     </>
   );
 };
