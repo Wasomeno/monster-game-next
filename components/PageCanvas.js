e3a54b693c8ca538ebe7194a0116756f8a0a9849
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-11 22:23:34 +0700

    Change: RegisterModal and added AnimatePresence

--- components/PageCanvas.js
+++ components/PageCanvas.js
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import AppContext from "./AppContext";
+import AppContext from "../contexts/AppContext";
 import PageComponents from "./PageComponents";
-import { motion } from "framer-motion";
-import NotConnected from "./NotConnected";
-import RegisterModal from "./RegisterModal";
+import { AnimatePresence, motion } from "framer-motion";
+import RegisterModal from "./modals/RegisterModal";
+
 const PageCanvas = ({ path }) => {
   const connection = useContext(AppContext);
   const isConnected = Boolean(connection.account[0]);
@@ -43,25 +43,20 @@ const PageCanvas = ({ path }) => {
   }, [drawCanvas]);
 
   return (
-    <motion.div
-      className="container-fluid"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ type: "tween", duration: 1 }}
-    >
-      {isConnected ? (
-        <>
-          <RegisterModal />
-          <div className="d-flex justify-content-center align-items-center">
-            <canvas ref={canvasRef} width={1000} height={window.innerHeight} />
-          </div>
-          <PageComponents path={path} />
-        </>
-      ) : (
-        <NotConnected />
-      )}
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        className="container-fluid"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ type: "tween", duration: 1 }}
+      >
+        <div className="d-flex justify-content-center align-items-center">
+          <canvas ref={canvasRef} width={1000} height={window.innerHeight} />
+        </div>
+        <PageComponents path={path} />
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
