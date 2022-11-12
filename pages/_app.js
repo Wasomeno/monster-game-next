c4071d1b6d7e637d2899550979e7ee2ccd7cd6e3
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-12 18:18:10 +0700

    Change: removed loading and toast from context

--- pages/_app.js
+++ pages/_app.js
@@ -1,54 +1,37 @@
 import { useState } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import LoadingScreen from "../components/LoadingScreen";
+import Toast from "../components/Toast";
 import Layout from "../components/Layout";
-import AppContext from "../components/AppContext";
-import { AnimatePresence } from "framer-motion";
-import { setToast } from "../components/Toast";
-import { setLoading } from "../components/LoadingScreen";
 import Head from "next/head";
+import { queryClient } from "../contexts/reactQueryClient";
+import AppContext from "../contexts/AppContext";
 
 function MyApp({ Component, pageProps }) {
   const [account, setAccount] = useState([]);
-  const [error, success, spinner, toggleSpinner, toggleToast, Toast] =
-    setToast();
-  const [setLoadingText, toggleLoading, Loading] = setLoading();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>Monsters Game</title>
         <meta property="og:title" content="My page title" key="title" />
       </Head>
-      <AnimatePresence exitBeforeEnter={true}>
-        <div className="bg-dark">
-          <AppContext.Provider
-            value={{
-              account: account,
-              setAccount: setAccount,
-              loading: {
-                setLoadingText: setLoadingText,
-                toggleLoading: toggleLoading,
-              },
-              toast: {
-                error: error,
-                success: success,
-                spinner: spinner,
-                toggleSpinner: toggleSpinner,
-                toggleToast: toggleToast,
-              },
-            }}
-          >
-            <Layout>
-              <Loading />
-              <Component {...pageProps} />
-              <Toast />
-            </Layout>
-          </AppContext.Provider>
-        </div>
-      </AnimatePresence>
-    </>
+      <div className="bg-dark">
+        <AppContext.Provider
+          value={{
+            account: account,
+            setAccount: setAccount,
+          }}
+        >
+          <Layout>
+            <LoadingScreen />
+            <Component {...pageProps} />
+            <Toast />
+          </Layout>
+        </AppContext.Provider>
+      </div>
+    </QueryClientProvider>
   );
 }
 
