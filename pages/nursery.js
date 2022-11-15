1d7bf8bf033e2277a14fe49b65a3754f049e1b16
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-15 19:25:19 +0700

    Removed unused imports

--- pages/nursery.js
+++ pages/nursery.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import PageCanvas from "../components/PageCanvas";
-import { usersDataContract } from "../hooks/useContract";
-import RegisterModal from "../components/RegisterModal";
 
 const Nursery = () => {
   const router = useRouter();
