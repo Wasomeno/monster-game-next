4537750c1d7c8db448d7d6e82ab0bbce59340f1f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-15 19:15:31 +0700

    Change: moved contract variables to functions

--- fetchers/fetchers.js
+++ fetchers/fetchers.js
@@ -5,40 +5,37 @@ import {
   nurseryContract,
   smelterContract,
   itemsContract,
+  monsterContract,
+  usersDataContract,
 } from "../hooks/useContract";
 
-const itemsHandler = itemsContract();
-const traderHandler = traderContract();
-const dungeonHandler = dungeonContract();
-const monsterGameHandler = monsterGameContract();
-const nurseryHandler = nurseryContract();
-const smelterHandler = smelterContract();
-
-export async function getShop() {
-  const shop = await traderHandler.getDailyShop();
-  return shop;
+export function getShop() {
+  const traderHandler = traderContract();
+  return async () => await traderHandler.getDailyShop();
 }
 
-export async function getTrades() {
-  const trader = await traderHandler.getDailyTrades();
-  return trader;
+export function getTrades() {
+  const traderHandler = traderContract();
+  return async () => await traderHandler.getDailyTrades();
 }
 
-export async function getMonstersOnDungeon(user) {
-  return await dungeonHandler.getMonstersOnDungeon(user);
+export function getMonstersOnDungeon(user) {
+  const dungeonHandler = dungeonContract();
+  return async () => await dungeonHandler.getMonstersOnDungeon(user);
 }
 
-export const missionTime = async (user) => {
+export const getMissionTime = async (user) => {
+  const monsterGameHandler = monsterGameContract();
   const details = await monsterGameHandler.monstersOnMissions(user);
   const timeStart = details.startTime;
   const timeRequired = parseInt(timeStart) + 15 * 60;
   const timeNow = Math.floor(Date.now() / 1000);
   const elapsedTime = (timeNow - timeRequired) / 60;
-
   return Math.floor(elapsedTime);
 };
 
 export const getDungeonTime = async (user) => {
+  const dungeonHandler = dungeonContract();
   const details = await dungeonHandler.monstersOnDungeon(user);
   const timeStart = details.startTime;
   const timeRequired = parseInt(timeStart) + 30 * 60;
@@ -48,7 +45,8 @@ export const getDungeonTime = async (user) => {
   return Math.floor(elapsedTime);
 };
 
-export const nurseryTime = async (user) => {
+export const getNurseryTime = async (user) => {
+  const nurseryHandler = nurseryContract();
   const details = await nurseryHandler.monstersOnNursery(user);
   const timeStart = parseInt(details.startTime);
   const duration = parseInt(details.duration);
@@ -59,7 +57,8 @@ export const nurseryTime = async (user) => {
   return Math.floor(elapsedTime);
 };
 
-export const smeltingTime = async (user) => {
+export const getSmeltingTime = async (user) => {
+  const smelterHandler = smelterContract();
   const details = await smelterHandler.smeltDetails(user);
   const timeStart = parseInt(details.startTime);
   const duration = parseInt(details.quantity) * 15 * 60;
@@ -70,10 +69,87 @@ export const smeltingTime = async (user) => {
   return Math.floor(elapsedTime);
 };
 
-export async function getPotion() {
-  return await itemsHandler.balanceOf(connection.account[0], 2);
+export function getPotion() {
+  const itemsHandler = itemsContract();
+  return async () => await itemsHandler.balanceOf(connection.account[0], 2);
 }
 
-export async function getInventory(user) {
-  return await itemsHandler.getInventory(user);
+export function getInventory(user) {
+  const itemsHandler = itemsContract();
+  return async () => await itemsHandler.getInventory(user);
+}
+
+export function getMonstersOnMissions(user) {
+  const monsterGameHandler = monsterGameContract();
+  return async () => await monsterGameHandler.getMonstersOnMission(user);
+}
+
+export async function getInactiveMonsters(user) {
+  const monstersHandler = monsterContract();
+  const monstersFetched = await monstersHandler.getMonsters(user);
+  const inactiveMonsters = await Promise.all(
+    monstersFetched.map(async (monster) => {
+      const stats = await monstersHandler.monsterStats(monster);
+      if (stats.status === 0) return { id: parseInt(monster), ...stats };
+    })
+  );
+  return inactiveMonsters.filter((details) => {
+    return details !== undefined;
+  });
+}
+
+export async function getAllMonsters(user) {
+  const monstersHandler = monsterContract();
+  const monstersFetched = await monstersHandler.getMonsters(user);
+  const details = await Promise.all(
+    monstersFetched.map(async (monster) => {
+      const { status } = await monstersHandler.monsterStats(monster);
+      return { id: monster, status: status };
+    })
+  );
+  return details;
+}
+
+export function getMonstersOnNursery(user) {
+  const nurseryHandler = nurseryContract();
+  return async () => await nurseryHandler.getRestingMonsters(user);
+}
+
+export function getCrystals(user) {
+  const itemsHandler = itemsContract();
+  return async () => await itemsHandler.balanceOf(user, 4);
+}
+
+export function getCrystalsInSmelter(user) {
+  const smelterHandler = smelterContract();
+  return async () => await smelterHandler.smeltDetails(user);
+}
+
+export function getApprovalStatus(user, contractAddress) {
+  const itemsHandler = itemsContract();
+  return async () => await itemsHandler.isApprovedForAll(user, contractAddress);
+}
+
+export function getGold(user) {
+  const itemsHandler = itemsContract();
+  return async () => await itemsHandler.balanceOf(user, 0);
+}
+
+export function getUserDetails(user) {
+  const userDataHandler = usersDataContract();
+  return async () => await userDataHandler.userDataDetails(user);
+}
+
+export function getUserStatus(user) {
+  const userDataHandler = usersDataContract();
+  return async () => await userDataHandler.registrationStatus(user);
+}
+export function getItemDetails(item) {
+  const traderHandler = traderContract();
+  return async () => await traderHandler.shopItems(item);
+}
+
+export function getDailyShopLimit(user) {
+  const traderHandler = traderContract();
+  return async () => await traderHandler.getShopDailyLimit(user);
 }
