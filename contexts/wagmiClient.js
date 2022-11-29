42824e87993c706172157d001472ea1d71a4835f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-29 21:06:38 +0700

    Wagmi client added

---
+++ contexts/wagmiClient.js
@@ -0,0 +1,17 @@
+import { createClient, configureChains, chain } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.goerli],
+  [infuraProvider({ apiKey: "10d1de5267e944c0a6580f6a690283a7" })]
+);
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [new MetaMaskConnector({ chains })],
+  provider,
+  webSocketProvider,
+});
+
+export default client;
