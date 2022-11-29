399b49e42cd58e29f555a8efe654f1f920d431f7
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-29 21:06:25 +0700

    Change from ethers Contract to wagmi useContract

--- hooks/useContract.js
+++ hooks/useContract.js
@@ -1,5 +1,3 @@
-import React from "react";
-import { ethers } from "ethers";
 import NurseryABI from "../abi/Nursery.json";
 import MonsterABI from "../abi/Monsters.json";
 import DungeonABI from "../abi/Dungeon.json";
@@ -8,96 +6,70 @@ import ItemsABI from "../abi/Items.json";
 import SmelterABI from "../abi/Smelter.json";
 import UsersDataABI from "../abi/UsersData.json";
 import MonsterGameABI from "../abi/MonsterGame.json";
+import { useContract, useProvider, useSigner } from "wagmi";
 
-export function nurseryContract() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const nurseryContract = new ethers.Contract(
-    process.env.NURSERY_CONTRACT_ADDRESS,
-    NurseryABI.abi,
-    signer
-  );
-  return nurseryContract;
-}
+const createContract = ({ contractAddress, abi }) => {
+  const provider = useProvider();
+  const { data: signer, isLoading } = useSigner();
+  return useContract({
+    address: contractAddress,
+    abi: abi,
+    signerOrProvider: !isLoading ? signer : provider,
+  });
+};
 
-export function monsterContract() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const monsterContract = new ethers.Contract(
-    process.env.MONSTER_CONTRACT_ADDRESS,
-    MonsterABI.abi,
-    signer
-  );
+export const monsterContract = () => {
+  return createContract({
+    contractAddress: process.env.MONSTER_CONTRACT_ADDRESS,
+    abi: MonsterABI.abi,
+  });
+};
 
-  return monsterContract;
-}
+export const monsterGameContract = () => {
+  return createContract({
+    contractAddress: process.env.MONSTER_GAME_CONTRACT_ADDRESS,
+    abi: MonsterGameABI.abi,
+  });
+};
 
-export function monsterGameContract() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const monsterGameContract = new ethers.Contract(
-    process.env.MONSTER_GAME_CONTRACT_ADDRESS,
-    MonsterGameABI.abi,
-    signer
-  );
+export const nurseryContract = () => {
+  return createContract({
+    contractAddress: process.env.NURSERY_CONTRACT_ADDRESS,
+    abi: NurseryABI.abi,
+  });
+};
 
-  return monsterGameContract;
-}
+export const dungeonContract = () => {
+  return createContract({
+    contractAddress: process.env.DUNGEON_CONTRACT_ADDRESS,
+    abi: DungeonABI.abi,
+  });
+};
 
-export function dungeonContract() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const dungeonContract = new ethers.Contract(
-    process.env.DUNGEON_CONTRACT_ADDRESS,
-    DungeonABI.abi,
-    signer
-  );
+export const itemsContract = () => {
+  return createContract({
+    contractAddress: process.env.ITEMS_CONTRACT_ADDRESS,
+    abi: ItemsABI.abi,
+  });
+};
 
-  return dungeonContract;
-}
+export const traderContract = () => {
+  return createContract({
+    contractAddress: process.env.TRADER_CONTRACT_ADDRESS,
+    abi: TraderABI.abi,
+  });
+};
 
-export function itemsContract() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const contract = new ethers.Contract(
-    process.env.ITEMS_CONTRACT_ADDRESS,
-    ItemsABI.abi,
-    signer
-  );
+export const smelterContract = () => {
+  return createContract({
+    contractAddress: process.env.SMELTER_CONTRACT_ADDRESS,
+    abi: SmelterABI.abi,
+  });
+};
 
-  return contract;
-}
-
-export function traderContract() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const traderContract = new ethers.Contract(
-    process.env.TRADER_CONTRACT_ADDRESS,
-    TraderABI.abi,
-    signer
-  );
-
-  return traderContract;
-}
-
-export function smelterContract() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const contract = new ethers.Contract(
-    process.env.SMELTER_CONTRACT_ADDRESS,
-    SmelterABI.abi,
-    signer
-  );
-  return contract;
-}
-
-export function usersDataContract() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const contract = new ethers.Contract(
-    process.env.USERS_DATA_CONTRACT_ADDRESS,
-    UsersDataABI.abi,
-    signer
-  );
-  return contract;
-}
+export const usersDataContract = () => {
+  return createContract({
+    contractAddress: process.env.USERS_DATA_CONTRACT_ADDRESS,
+    abi: UsersDataABI.abi,
+  });
+};
