a402a47a020ab5221c80b398f52a4dec902a43f1
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-30 23:44:11 +0700

    Change isConnected to wagmi hook & class names to tailwind

--- components/PageCanvas.js
+++ components/PageCanvas.js
@@ -2,11 +2,10 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import AppContext from "../contexts/AppContext";
 import PageComponents from "./PageComponents";
 import { AnimatePresence, motion } from "framer-motion";
-import RegisterModal from "./modals/RegisterModal";
+import { useAccount } from "wagmi";
 
 const PageCanvas = ({ path }) => {
-  const connection = useContext(AppContext);
-  const isConnected = Boolean(connection.account[0]);
+  const { isConnected } = useAccount();
   const [image, setImage] = useState(null);
   const canvasRef = useRef(null);
 
@@ -15,13 +14,11 @@ const PageCanvas = ({ path }) => {
   };
 
   useEffect(() => {
-    if (isConnected) {
-      const image = new Image();
-      image.src = "/canvas_images/" + path + ".png";
-      image.onload = () => {
-        setImage(image);
-      };
-    }
+    const image = new Image();
+    image.src = "/canvas_images/" + path + ".png";
+    image.onload = () => {
+      setImage(image);
+    };
   }, [isConnected]);
 
   useEffect(() => {
@@ -45,13 +42,13 @@ const PageCanvas = ({ path }) => {
   return (
     <AnimatePresence>
       <motion.div
-        className="container-fluid"
+        className="h-screen bg-slate-800"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ type: "tween", duration: 1 }}
       >
-        <div className="d-flex justify-content-center align-items-center">
+        <div className="flex justify-center items-center">
           <canvas ref={canvasRef} width={1000} height={window.innerHeight} />
         </div>
         <PageComponents path={path} />
