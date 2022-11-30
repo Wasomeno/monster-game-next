6c4072290a9566bb444d7e20005b8d423bd16b01
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-30 23:43:26 +0700

    Added in component BackButton

--- components/Modal.js
+++ components/Modal.js
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion";
+import { BackButton } from "./Buttons/Buttons";
 
-const Modal = ({ show, children }) => {
+const Modal = ({ show, toggleShow, children }) => {
   return (
     <AnimatePresence>
       {show && (
@@ -13,12 +14,13 @@ const Modal = ({ show, children }) => {
             transition={{ type: "tween", duration: 0.25 }}
           />
           <motion.div
-            className="modal absolute z-15 bg-slate-900 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-md"
+            className="modal p-3 absolute z-15 bg-slate-900 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-md"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ type: "tween", duration: 0.25 }}
           >
+            <BackButton onClick={() => toggleShow()} />
             {children}
           </motion.div>
         </>
