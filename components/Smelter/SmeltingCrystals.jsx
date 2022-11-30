9a6f4039299f2dcdc5598efda487f40bdcd99866
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-30 23:56:41 +0700

    Changed mutation & query to custom hooks

--- components/Smelter/SmeltingCrystals.jsx
+++ components/Smelter/SmeltingCrystals.jsx
@@ -1,17 +1,12 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import React from "react";
-import { getCrystalsInSmelter } from "../../fetchers/fetchers";
-import { finishSmelt } from "../../mutations/mutations";
-import TimeButton from "../TimeButton";
+import { useCrystals } from "../../fetchers/useSmelter";
+import { finishSmelt } from "../../mutations/smelterMutations";
+import TimeButton from "../Buttons/TimeButton";
 
-const SmeltingCrystals = ({ user }) => {
-  const crystalsInSmelter = useQuery(
-    ["cyrstalsInSmelter", user],
-    getCrystalsInSmelter(user)
-  );
-  const finishSmelting = useMutation(() => finishSmelt());
-
+const SmeltingCrystals = () => {
+  const crystalsInSmelter = useCrystals({ key: "smelter" });
+  const finishSmelting = finishSmelt();
   return (
     <div className="w-4/12 text-center">
       <h5 className="m-0 text-white p-2">Crystals in Smelter</h5>
@@ -27,7 +22,7 @@ const SmeltingCrystals = ({ user }) => {
           x {crystalsInSmelter.data?.toString()}
         </h5>
       </div>
-      <TimeButton path={"smelter"} onClick={() => finishSmelting.mutate()} />
+      <TimeButton activity="smelter" onClick={() => finishSmelting.mutate()} />
     </div>
   );
 };
