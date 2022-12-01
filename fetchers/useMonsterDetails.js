7e4142da8869834ba16c4be4226a2fec7975337c
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-02 00:09:51 +0700

    Added get monsterDetails hook

---
+++ fetchers/useMonsterDetails.js
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { monsterContract } from "../hooks/useContract";
+
+const useMonsterDetails = ({ monster }) => {
+  const monstersHandler = monsterContract();
+  const { data, isLoading, isError } = useQuery(
+    ["monsterDetails", monster],
+    async () => {
+      return await monstersHandler.monsterStats(monster);
+    }
+  );
+
+  return { data: data, isLoading: isLoading, isError: isError };
+};
+
+export default useMonsterDetails;
