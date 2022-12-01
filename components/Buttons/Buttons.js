cdd597cb68b4ca72545465f149f8bc921824f0ea
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-01 23:58:17 +0700

    Added custom size classes props for StatActivityButton

--- components/Buttons/Buttons.js
+++ components/Buttons/Buttons.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
 
-export const StartActivityButton = ({ text, onClick, condition }) => {
+const sizeClasses = {
+  default: "w-3/12",
+  medium: "w-6/12",
+  large: "w-9/12",
+};
+
+export const StartActivityButton = ({ text, onClick, condition, size }) => {
   return (
     <button
       disabled={condition}
-      className="bg-green-700 rounded-md tracking-wide p-2 w-3/12 m-2 font-monogram transition ease-in-out duration-300 text-white text-xl hover:bg-slate-50 hover:text-black disabled:bg-opacity-30 disabled:text-opacity-30"
+      className={
+        (!size ? sizeClasses["default"] : sizeClasses[size]) +
+        " bg-green-700 rounded-md tracking-wide p-2 m-2 font-monogram transition ease-in-out duration-300 text-white text-xl hover:bg-slate-50 hover:text-black disabled:bg-opacity-30 disabled:text-opacity-30"
+      }
       onClick={onClick}
     >
       {text}
