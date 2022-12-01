7957827e191d6fdf2dc9c26160e857e8c69d5101
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-02 00:07:25 +0700

    Changed  crystalsInInventory query to query hook & same with the smelt mutation

--- components/Smelter/UserCrystalsSection.jsx
+++ components/Smelter/UserCrystalsSection.jsx
@@ -1,25 +1,14 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import React, { useState } from "react";
-import { getApprovalStatus, getCrystals } from "../../fetchers/fetchers";
-import { smelt, smeltNotApproved } from "../../mutations/mutations";
+import { useCrystals } from "../../fetchers/useSmelter";
+import { useSmelt } from "../../mutations/smelterMutations";
 import { StartActivityButton } from "../Buttons/Buttons";
 
-const UserCrystalsSection = ({ user }) => {
-  const [crystals, setCrystals] = useState(0);
-  const crytalsInInventory = useQuery(
-    ["crystalsInInventory", user],
-    getCrystals(user)
-  );
+const UserCrystalsSection = () => {
+  const [crystalsAmount, setCrystalsAmount] = useState(0);
+  const crystalsInInventory = useCrystals({ key: "inventory" });
+  const startSmelt = useSmelt(crystalsAmount);
 
-  const approvalStatus = useQuery(
-    ["approvalStatus", process.env.SMELTER_CONTRACT_ADDRESS],
-    getApprovalStatus(user, process.env.SMELTER_CONTRACT_ADDRESS)
-  );
-
-  const startSmelt = useMutation(() =>
-    approvalStatus.data ? smelt(crystals) : smeltNotApproved(crystals)
-  );
   return (
     <div className="w-4/12 text-center">
       <h5 className="m-0 text-white p-2">Your Crystals</h5>
@@ -36,21 +25,21 @@ const UserCrystalsSection = ({ user }) => {
           <input
             type="text"
             className="w-4/12 text-center rounded m-0 mx-2 p-1"
-            value={crystals}
+            value={crystalsAmount}
             onChange={(input) =>
-              crystals > parseInt(crytalsInInventory.data)
-                ? setCrystals(parseInt(crytalsInInventory.data))
-                : setCrystals(input.target.value)
+              crystalsAmount > parseInt(crystalsInInventory.data)
+                ? setCrystalsAmount(parseInt(crystalsInInventory.data))
+                : setCrystalsAmount(input.target.value)
             }
           />
           <h5 className="m-0 p-2 text-white">
-            / {crytalsInInventory.data?.toString()}
+            / {crystalsInInventory.data.toString()}
           </h5>
         </div>
 
         <h5 className="col-2 m-0 text-white">{">"}</h5>
       </div>
-      <StartActivityButton text="Smelt" onClick={() => startSmelt.mutate()} />
+      <StartActivityButton text="Smelt" onClick={() => startSmelt()} />
     </div>
   );
 };
