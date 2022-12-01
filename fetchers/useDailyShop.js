3a52c633dfbd0744277a39ed75e95a00c1c250b2
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-02 00:08:50 +0700

    Added userDailyShopLimit hook

--- fetchers/useDailyShop.js
+++ fetchers/useDailyShop.js
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { useAccount } from "wagmi";
 import { traderContract } from "../hooks/useContract";
 
 const useDailyShop = () => {
@@ -21,6 +22,19 @@ const useDailyShop = () => {
     });
     return itemsWithDetails;
   });
+  return { data: data, isLoading: isLoading, isError: isError };
+};
+
+export const useUserDailyShopLimit = () => {
+  const { address: user } = useAccount();
+  const traderHandler = traderContract();
+  const { data, isLoading, isError } = useQuery(
+    ["dailyShopLimit", user],
+    async () => {
+      const limit = await traderHandler.getShopDailyLimit(user);
+      return limit;
+    }
+  );
   return { data: data, isLoading: isLoading, isError: isError };
 };
 
