f4b93f9a059c85ec5d632b3532ad22477cdb3257
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-02 00:00:31 +0700

    Changed trade mutation to mutation hook & removed unused props

--- components/DailyTrader/TradeDetails.jsx
+++ components/DailyTrader/TradeDetails.jsx
@@ -1,26 +1,14 @@
 import Image from "next/image";
 import React, { useState } from "react";
-import { useMutation } from "@tanstack/react-query";
 import TradeAmountControl from "./TradeAmountControl";
 import { items } from "./itemsMap";
 import { Paragraph } from "../Texts";
-import { tradeSides } from "../../mutations/sideffects";
-import {
-  tradeItemApproved,
-  tradeItemNotApproved,
-} from "../../mutations/mutations";
+import { useTrade } from "../../mutations/traderMutations";
 
-const TradeDetails = ({ details, approval, user }) => {
-  const { id, trade } = details;
+const TradeDetails = ({ details }) => {
+  const { id, tradeDetails } = details;
   const [quantity, setQuantity] = useState(1);
-  const tradeMutation = useMutation(
-    () =>
-      approval
-        ? tradeItemApproved(id, quantity, user)
-        : tradeItemNotApproved(id, quantity, user),
-    tradeSides()
-  );
-
+  const trade = useTrade({ id: id, quantity: quantity });
   return (
     <div className="flex justify-evenly items-center w-full h-18">
       <div className="flex flex-col justify-evenly items-center">
@@ -28,12 +16,13 @@ const TradeDetails = ({ details, approval, user }) => {
           <Image
             width={40}
             height={40}
-            src={items.get(trade.itemReceived).image}
+            src={items.get(tradeDetails.itemReceived).image}
           />
         </div>
         <div>
           <Paragraph>
-            {items.get(trade.itemReceived).name} x{trade.quantityReceived}
+            {items.get(tradeDetails.itemReceived).name} x
+            {tradeDetails.quantityReceived}
           </Paragraph>
         </div>
       </div>
@@ -42,23 +31,24 @@ const TradeDetails = ({ details, approval, user }) => {
           <Image
             width={40}
             height={40}
-            src={items.get(trade.itemTrade).image}
+            src={items.get(tradeDetails.itemTrade).image}
           />
         </div>
         <div>
           <Paragraph>
-            {items.get(trade.itemTrade).name} x{trade.quantityTrade}
+            {items.get(tradeDetails.itemTrade).name} x
+            {tradeDetails.quantityTrade}
           </Paragraph>
         </div>
       </div>
       <TradeAmountControl
         amount={quantity}
         setAmount={setQuantity}
-        limit={trade.limit}
+        limit={tradeDetails.limit}
       />
       <button
         className="font-monogram p-1 w-1/12 rounded-md bg-slate-50 text-lg"
-        onClick={() => tradeMutation.mutate()}
+        onClick={() => trade()}
       >
         Trade
       </button>
