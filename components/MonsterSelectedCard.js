2ac75c7c6228e4d965134c19cac61b38eeab2c72
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-03 04:17:21 +0700

    Added a border-4

--- components/MonsterSelectedCard.js
+++ components/MonsterSelectedCard.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const MonsterSelectedCard = ({ monster }) => {
   return (
-    <div className="w-20 bg-slate-600 p-2 text-center d-flex justify-content-center rounded align-items-center">
+    <div className="w-20 border-4 border-slate-500 bg-slate-700 p-2 text-center d-flex justify-content-center rounded align-items-center">
       <Image
         src={"/monsters/" + parseInt(monster + 1) + ".png"}
         width="200"
