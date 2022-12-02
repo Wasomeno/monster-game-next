a8bf40c8b769eb3d34701962b532cddab112bd56
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-03 04:24:10 +0700

    Added a Stat component for displaying monster stats

---
+++ components/Monsters/Stat.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Paragraph } from "../Texts";
+import StatsBar from "./StatsBar";
+
+const Stat = ({ stat, bar, value, maxValue }) => {
+  return (
+    <div className="flex flex-col items-center justify-evenly">
+      <Paragraph>{stat}</Paragraph>
+      {bar && <StatsBar value={value} maxValue={maxValue} />}
+      {!bar && (
+        <div className="bg-slate-700 p-1 px-3 rounded-md">
+          <Paragraph>{value}</Paragraph>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Stat;
