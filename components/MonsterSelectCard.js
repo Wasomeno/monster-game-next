282ac0a65a091140acfe4636508d510ed250cbcc
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-03 04:17:01 +0700

    Moved multiple props into one props

--- components/MonsterSelectCard.js
+++ components/MonsterSelectCard.js
@@ -1,22 +1,19 @@
 import Image from "next/image";
 
-const MonsterSelectCard = ({
-  monster,
-  level,
-  exp,
-  expCap,
-  energy,
-  energyCap,
-  onClick,
-}) => {
+const MonsterSelectCard = ({ stats, onClick }) => {
   return (
     <div
-      className="w-36 flex flex-col justify-center items-center bg-slate-100 bg-opacity-40 rounded-md cursor-pointer transition duration-300 ease-in-out hover:bg-slate-600"
+      className="w-44 h-80 p-2 border-4 border-slate-500 flex flex-col justify-center items-center bg-slate-700 bg-opacity-40 rounded-md cursor-pointer transition duration-300 ease-in-out hover:bg-slate-600"
       onClick={onClick}
     >
-      <div className="m-1 p-2 flex items-center">
+      <div>
+        <h5 className="text-white font-monogram text-xl text-center">
+          Monster #{stats.id}
+        </h5>
+      </div>
+      <div className="m-1 p-3 flex items-center bg-slate-700 rounded">
         <Image
-          src={"/monsters/" + (parseInt(monster) + 1) + ".png"}
+          src={"/monsters/" + (parseInt(stats.id) + 1) + ".png"}
           alt="monster-img"
           width="150"
           height="150"
@@ -24,18 +21,24 @@ const MonsterSelectCard = ({
         />
       </div>
 
-      <div className="m-2 py-1">
-        <h5 className="text-white font-monogram text-2xl text-center">
-          Monster #{monster}
-        </h5>
+      <div className="w-full m-2 py-1">
         <div className="text-start font-monogram text-white text-lg">
-          <h5>Level : {level} / 10</h5>
-          <h5>
-            Exp : {exp} / {expCap}
-          </h5>
-          <h5>
-            Energy : {energy} / {energyCap}
-          </h5>
+          <div className="flex justify-evenly">
+            <h5 className="w-5/12 text-center">Level</h5>
+            <h5 className="w-5/12 text-center">{stats.level} / 10</h5>
+          </div>
+          <div className="flex justify-evenly">
+            <h5 className="w-4/12 text-center">Exp</h5>
+            <h5 className="w-5/12 text-center">
+              {stats.exp} / {stats.expCap}
+            </h5>
+          </div>
+          <div className="flex justify-evenly">
+            <h5 className="w-4/12 text-center">Energy</h5>
+            <h5 className="w-5/12 text-center">
+              {stats.energy} / {stats.energyCap}
+            </h5>
+          </div>
         </div>
       </div>
     </div>
