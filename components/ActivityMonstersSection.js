3d55bd8d56754bf8c3f4d06ed5e80cc9be39d6d7
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-04 04:30:15 +0700

    Added render condition when loading

--- components/ActivityMonstersSection.js
+++ components/ActivityMonstersSection.js
@@ -1,12 +1,16 @@
 import React from "react";
 import MonsterSelectedCard from "./MonsterSelectedCard";
+import MoonLoader from "react-spinners/MoonLoader";
 import { Paragraph } from "./Texts";
 
 const ActivityMonstersSection = ({ monsterSelected, monstersOnActivity }) => {
+  const { data: monsters, isLoading, isError } = monstersOnActivity;
   return (
     <div className="p-3 mx-2 w-6/12 h-28 flex gap-2 justify-center items-center border border-light border-opacity-25 rounded-md">
-      {monstersOnActivity.length < 1 ? (
-        monsterSelected.length !== 0 ? (
+      {isLoading ? (
+        <MoonLoader loading={isLoading} color="white" size={20} />
+      ) : monsters.length < 1 ? (
+        monsterSelected?.length !== 0 ? (
           monsterSelected.map((monster, index) => (
             <MonsterSelectedCard key={index} monster={monster} />
           ))
@@ -14,7 +18,7 @@ const ActivityMonstersSection = ({ monsterSelected, monstersOnActivity }) => {
           <Paragraph>No Monster Selected</Paragraph>
         )
       ) : (
-        monstersOnActivity.map((monster, index) => (
+        monsters.map((monster, index) => (
           <MonsterSelectedCard key={index} monster={parseInt(monster)} />
         ))
       )}
