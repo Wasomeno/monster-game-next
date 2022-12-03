41896681d8b0f7c248bea4ab1fd5c174ebc53e5e
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-04 04:36:26 +0700

    Removed user details context

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
-import React, { useEffect, useRef, useState, useContext } from "react";
-import { motion } from "framer-motion";
-import Head from "next/head";
-import AppContext from "../components/AppContext";
-import Link from "next/link";
-import MapButton from "../components/buttons/MapButton";
+import React, { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import MapButton from "../components/Buttons/MapButton";
 
 const Map = () => {
-  const connection = useContext(AppContext);
-  const isConnected = Boolean(connection.account[0]);
   const canvasRef = useRef(null);
   const [image, setImage] = useState(null);
 
@@ -23,14 +18,12 @@ const Map = () => {
   };
 
   useEffect(() => {
-    if (isConnected) {
-      const mapImage = new Image();
-      mapImage.src = "/canvas_images/map.png";
-      mapImage.onload = () => {
-        setImage(mapImage);
-      };
-    }
-  }, [isConnected]);
+    const mapImage = new Image();
+    mapImage.src = "/canvas_images/map.png";
+    mapImage.onload = () => {
+      setImage(mapImage);
+    };
+  }, []);
 
   useEffect(() => {
     if (image && canvasRef) {
@@ -52,26 +45,24 @@ const Map = () => {
     }
   }, [drawCanvas]);
 
-  if (!isConnected) return;
-
   return (
-    <>
+    <AnimatePresence>
       <motion.div
-        id="map-container"
+        className="relative h-screen w-screen bg-sea-blue"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ type: "tween", duration: 1 }}
       >
-        <div className="d-flex justify-content-center align-items-center">
-          <canvas ref={canvasRef} className="map-canvas" />
+        <div className="h-screen flex justify-center items-center">
+          <canvas ref={canvasRef} className="h-screen w-screen" />
         </div>
         <MapButton link={"cityhall"} y={"240px"} x={"53%"} />
         <MapButton link={"dungeon"} y={"500px"} x={"24.5%"} />
         <MapButton link={"nursery"} y={"490px"} x={"72.5%"} />
         <MapButton link={"altar"} y={"220px"} x={"27.5%"} />
       </motion.div>
-    </>
+    </AnimatePresence>
   );
 };
 
