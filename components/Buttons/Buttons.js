b4b705771f26793ed195c961b666fd1a6e4e4d70
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-04 04:32:44 +0700

    Added loading condition for StartActivityButton

--- components/Buttons/Buttons.js
+++ components/Buttons/Buttons.js
@@ -1,22 +1,32 @@
 import Image from "next/image";
-
+import MoonLoader from "react-spinners/MoonLoader";
 const sizeClasses = {
   default: "w-3/12",
   medium: "w-6/12",
   large: "w-9/12",
 };
 
-export const StartActivityButton = ({ text, onClick, condition, size }) => {
+export const StartActivityButton = ({
+  text,
+  onClick,
+  condition,
+  size,
+  loading,
+}) => {
   return (
     <button
       disabled={condition}
       className={
         (!size ? sizeClasses["default"] : sizeClasses[size]) +
-        " bg-green-700 rounded-md tracking-wide p-2 m-2 font-monogram transition ease-in-out duration-300 text-white text-xl hover:bg-slate-50 hover:text-black disabled:bg-opacity-30 disabled:text-opacity-30"
+        " flex justify-center bg-green-700 rounded-md tracking-wide p-2 m-2 font-monogram transition ease-in-out duration-300 text-white text-xl hover:bg-slate-50 hover:text-black disabled:bg-opacity-30 disabled:text-opacity-30"
       }
       onClick={onClick}
     >
-      {text}
+      {loading ? (
+        <MoonLoader loading={loading} color="white" size={20} />
+      ) : (
+        text
+      )}
     </button>
   );
 };
@@ -24,7 +34,7 @@ export const StartActivityButton = ({ text, onClick, condition, size }) => {
 export const DangerButton = ({ text, onClick, condition }) => {
   return (
     <button
-      disabled={!condition}
+      disabled={condition}
       className="bg-red-700 rounded-md tracking-wide p-2 w-3/12 m-2 font-monogram transition ease-in-out duration-300 text-white text-xl hover:bg-slate-50 hover:text-black disabled:bg-opacity-30 disabled:text-opacity-30"
       onClick={onClick}
     >
