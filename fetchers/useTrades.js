fa58f5fc9d6d607668645f858b46400b17bc513f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-05 12:34:45 +0700

    Added useUserDailyTradeLimit for getting the user daily trade limit

--- fetchers/useTrades.js
+++ fetchers/useTrades.js
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { useAccount } from "wagmi";
 import { traderContract } from "../hooks/useContract";
 
 const useTrades = () => {
@@ -6,6 +7,19 @@ const useTrades = () => {
   const { data, isLoading, isError } = useQuery(["trades"], async () => {
     return await traderHandler.getDailyTrades();
   });
+
+  return { data: data, isLoading: isLoading, isError: isError };
+};
+
+export const useUserDailyTradeLimit = () => {
+  const { address: user } = useAccount();
+  const traderHandler = traderContract();
+  const { data, isLoading, isError } = useQuery(
+    ["dailyTradeLimit", user],
+    async () => {
+      return await traderHandler.getTradeDailyLimit(user);
+    }
+  );
 
   return { data: data, isLoading: isLoading, isError: isError };
 };
