4eb75145f3dcdd9d7b2667017ca6b76ab0241687
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-05 12:33:53 +0700

    Changed the styles for displaying the user crystals in inventory

--- components/Smelter/UserCrystalsSection.jsx
+++ components/Smelter/UserCrystalsSection.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { useCrystals } from "../../fetchers/useSmelter";
 import { useSmelt } from "../../mutations/smelterMutations";
 import { StartActivityButton } from "../Buttons/Buttons";
+import { Paragraph } from "../Texts";
 
 const UserCrystalsSection = () => {
   const [crystalsAmount, setCrystalsAmount] = useState(0);
@@ -10,36 +11,30 @@ const UserCrystalsSection = () => {
   const startSmelt = useSmelt(crystalsAmount);
 
   return (
-    <div className="w-4/12 text-center">
-      <h5 className="m-0 text-white p-2">Your Crystals</h5>
-      <div className="flex justify-around items-center my-2">
-        <h5 className="m-0 text-white col-2">{"<"}</h5>
-        <div className="flex justify-center items-center">
-          <Image
-            src="/items/4.png"
-            width={"40"}
-            height={"40"}
-            alt="crystal-img"
-            className="my-2"
-          />
-          <input
-            type="text"
-            className="w-4/12 text-center rounded m-0 mx-2 p-1"
-            value={crystalsAmount}
-            onChange={(input) =>
-              crystalsAmount > parseInt(crystalsInInventory.data)
-                ? setCrystalsAmount(parseInt(crystalsInInventory.data))
-                : setCrystalsAmount(input.target.value)
-            }
-          />
-          <h5 className="m-0 p-2 text-white">
-            / {crystalsInInventory.data.toString()}
-          </h5>
+    <div className="w-4/12 text-center flex flex-col items-center gap-2">
+      <Paragraph>Your Crystals</Paragraph>
+      <div className="w-4/6 flex justify-center items-center my-2">
+        <button className="bg-slate-50 p-1 w-8 h-8 rounded font-monogram text-xl flex justify-center items-center">
+          {"<"}
+        </button>
+        <div className="flex justify-center items-center w-3/6">
+          <Paragraph>
+            {crystalsAmount} / {crystalsInInventory.data?.toString()}
+          </Paragraph>
         </div>
 
-        <h5 className="col-2 m-0 text-white">{">"}</h5>
+        <button className="bg-slate-50 p-1 w-8 h-8 rounded font-monogram text-xl flex justify-center items-center">
+          {">"}
+        </button>
       </div>
-      <StartActivityButton text="Smelt" onClick={() => startSmelt()} />
+      <div className="flex justify-center w-full">
+        <StartActivityButton
+          text="Smelt"
+          onClick={() => startSmelt()}
+          condition={crystalsAmount < 1}
+          size="medium"
+        />
+      </div>
     </div>
   );
 };
