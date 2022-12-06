0caea93f5045ee67ee9c6cda0ff1475386cbd1a8
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-06 23:52:21 +0700

    Added function for invalidate query

--- contexts/reactQueryClient.js
+++ contexts/reactQueryClient.js
@@ -1,3 +1,6 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
+
+export const invalidateQuery = (queryKey) =>
+  queryClient.invalidateQueries(queryKey);
